b3e23b6bbe05d62f76a5d2abaad380226cd1d16e
Sachinmd82 <sachin.md@kapturecrm.com>
2023-10-27 20:33:29 +0530

    Added Login controller for admin

---
+++ SprintMaster/src/main/java/com/example/SprintMaster/Controller/LoginController.java
@@ -0,0 +1,35 @@
+package com.example.SprintMaster.Controller;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import com.example.SprintMaster.Service.AdminService;
+
+@Controller
+public class LoginController {
+	@Autowired
+	AdminService adminService;
+	
+	@GetMapping("/validate-user")
+    public ResponseEntity<?> validateUser(@RequestHeader("Authorization") String authorizationHeader) {
+    	Map<String, Object> map = new LinkedHashMap<String, Object>();
+//        boolean isValidUser = adminService.validateUser(username, password);
+        if (adminService.validateUser(authorizationHeader)) {
+        	map.put("Status", "Success");
+        	map.put("Message", "Valid User!");
+        	return new ResponseEntity<>(map,HttpStatus.OK);
+        } 
+        map.put("Status", "Success");
+    	map.put("Message", "Invalid username or password.");
+    	return new ResponseEntity<>(map,HttpStatus.UNAUTHORIZED);        
+    }
+
+
+}
--- SprintMaster/src/main/java/com/example/SprintMaster/Repository/EmployeeRepository.java
+++ SprintMaster/src/main/java/com/example/SprintMaster/Repository/EmployeeRepository.java
@@ -1,5 +1,13 @@
 package com.example.SprintMaster.Repository;
 
-public class EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.SprintMaster.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	Employee findByUsername(String username);
 
 }
