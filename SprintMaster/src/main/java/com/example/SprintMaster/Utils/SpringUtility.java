d78dd1834ed02c471bed704359752418081eb3aa
basavarajlpb <basavaraj.lp@kapturecrm.com>
2023-10-27 21:38:45 +0530

    Logger Activity String

--- SprintMaster/src/main/java/com/example/SprintMaster/Utils/SpringUtility.java
+++ SprintMaster/src/main/java/com/example/SprintMaster/Utils/SpringUtility.java
@@ -2,4 +2,8 @@ package com.example.SprintMaster.Utils;
 
 public class SpringUtility {
 
+	public static final String ASSIGNED			="Assigned";
+	public static final String BREAK_START		="Break-Start";
+	public static final String BREAK_END 		="Break-End";
+	public static final String DONE 			="Done";
 }
