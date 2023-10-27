0d72e466dd09c2db192bff4d3ecffba6ea210759
Sachinmd82 <sachin.md@kapturecrm.com>
2023-10-27 18:30:58 +0530

    First commit

---
+++ SprintMaster/src/main/java/com/example/SprintMaster/SprintMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.SprintMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintMasterApplication.class, args);
+	}
+
+}
---
+++ SprintMaster/src/test/java/com/example/SprintMaster/SprintMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SprintMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
